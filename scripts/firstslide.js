c2c30915d0524029a7f3b7f20512b244b687d217
DieNice <DailyPlanning@bk.ru>
2020-01-02 01:09:31 +1000

    add control form for first clide

---
+++ scripts/firstslide.js
@@ -0,0 +1,23 @@
+let i = 0;
+function buttonno() {
+    i++;
+    i = i % 4;
+    switch (i) {
+        case 0:
+            caption = document.getElementById('playcaption').innerHTML = "Вы готовы?";
+            break;
+        case 1:
+            caption = document.getElementById('playcaption').innerHTML = "А теперь?";
+            break;
+        case 2:
+            caption = document.getElementById('playcaption').innerHTML = "А сейчас?";
+            break;
+        case 3:
+            caption = document.getElementById('playcaption').innerHTML = "Ну и ладно:(";
+            break;
+    }
+}
+
+function getName() {
+    
+}
